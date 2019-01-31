be8d6edf1ff88b5273b36404e6ed8fdbff997b8b
Bedapudi Praneeth <praneethbedapudi@gmail.com>
2019-01-31 15:16:17 +0530

    minor performace updates and bug corrections

--- seqtag/__init__.py
+++ seqtag/__init__.py
@@ -1,1 +1,2 @@
-__version__ = '1.0.2'
+__version__ = '1.0.7'
+__author__ = 'Originally authored by https://github.com/guillaumegenthial. Packaged and imporvements, bug corrections by Bedapudi Praneeth (@bedapudi6788)'
